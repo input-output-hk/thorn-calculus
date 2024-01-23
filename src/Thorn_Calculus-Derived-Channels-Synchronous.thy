6fe0645904f17390446a9d1838839a210555f9d6
Wolfgang Jeltsch <wolfgang@well-typed.com>
2024-01-23 18:26:03 +0200

    Fix copyright and licensing information

--- src/Thorn_Calculus-Derived-Channels-Synchronous.thy
+++ src/Thorn_Calculus-Derived-Channels-Synchronous.thy
@@ -1,3 +1,17 @@
+(*
+  Copyright 2021–2024 Input Output Global Inc.
+
+  Licensed under the Apache License, Version 2.0 (the “License”); you may not use this file except
+  in compliance with the License. You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software distributed under the License
+  is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+  or implied. See the License for the specific language governing permissions and limitations under
+  the License.
+*)
+
 text \<open>
   The technique used here is by Honda and Tokoro. See Example~1.6.2 in the \<open>\<pi>\<close>-dist thesis by
   Vyšniauskas and the paper \<^emph>\<open>An Object Calculus for Asynchronous Communication\<close> by Honda and Tokoro
